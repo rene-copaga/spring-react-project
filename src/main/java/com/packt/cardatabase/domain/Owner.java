0df0bf9caaf8dbc2cdc630bcce1b8f39ca8a59ad
Rene Copaga <rene.copaga@gmail.com>
2021-02-21 18:26:12 -0400

    Creating a RESTful web service

--- src/main/java/com/packt/cardatabase/domain/Owner.java
+++ src/main/java/com/packt/cardatabase/domain/Owner.java
@@ -1,9 +1,13 @@
 package com.packt.cardatabase.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Owner {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -11,6 +15,7 @@ public class Owner {
     private String firstname, lastname;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    @JsonIgnore
     private List<Car> cars;
 
     public Owner() {}
