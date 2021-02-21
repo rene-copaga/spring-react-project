03e23fa8cd6f5a94a01216f2f41cd45dc5fedbc7
Rene Copaga <rene.copaga@gmail.com>
2021-02-21 13:08:11 -0400

    Relationships between tables

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,5 +1,7 @@
 package com.packt.cardatabase;
 
+import com.packt.cardatabase.domain.Owner;
+import com.packt.cardatabase.domain.OwnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,9 @@ public class CardatabaseApplication {
 	@Autowired
 	private CarRepository repository;
 
+	@Autowired
+	private OwnerRepository orepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
 	}
@@ -22,13 +27,18 @@ public class CardatabaseApplication {
 	@Bean
 	CommandLineRunner runner(){
 		return args -> {
+			// Add owner objects and save these to db
+			Owner owner1 = new Owner("John" , "Johnson");
+			Owner owner2 = new Owner("Mary" , "Robinson");
+			orepository.save(owner1);
+			orepository.save(owner2);
 			// Save demo data to database
 			repository.save(new Car("Ford", "Mustang", "Red",
-					"ADF-1121", 2017, 59000));
+					"ADF-1121", 2017, 59000, owner1));
 			repository.save(new Car("Nissan", "Leaf", "White",
-					"SSJ-3002", 2014, 29000));
+					"SSJ-3002", 2014, 29000, owner2));
 			repository.save(new Car("Toyota", "Prius", "Silver",
-					"KKO-0212", 2018, 39000));
+					"KKO-0212", 2018, 39000, owner2));
 		};
 	}
 }
--- src/main/java/com/packt/cardatabase/domain/Car.java
+++ src/main/java/com/packt/cardatabase/domain/Car.java
@@ -1,9 +1,6 @@
 package com.packt.cardatabase.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Car {
@@ -13,9 +10,13 @@ public class Car {
 	private String brand, model, color, registerNumber;
 	private int year, price;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "owner")
+	private Owner owner;
+
 	public Car() {}
 	public Car(String brand, String model, String color,
-			   String registerNumber, int year, int price) {
+			   String registerNumber, int year, int price, Owner owner) {
 		super();
 		this.brand = brand;
 		this.model = model;
@@ -23,6 +24,7 @@ public class Car {
 		this.registerNumber = registerNumber;
 		this.year = year;
 		this.price = price;
+		this.owner = owner;
 	}
 
 	public String getBrand() {
@@ -60,5 +62,12 @@ public class Car {
 	}
 	public void setPrice(int price) {
 		this.price = price;
+	}
+
+	public Owner getOwner() {
+		return owner;
+	}
+	public void setOwner(Owner owner) {
+		this.owner = owner;
 	}
 }
